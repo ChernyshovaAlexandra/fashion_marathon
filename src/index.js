7374fe54d0ce8170795bee5832a29a360141a880
Alexandra <alexandrachrystal@gmail.com>
2024-06-05 14:29:18 +0700

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './fonts/stylesheet.css'
-import './fonts/BebasNeue/stylesheet.css'
-import './css/main.css'
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
